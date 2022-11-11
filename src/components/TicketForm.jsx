277b8a3a6f4febb63375739855455eb64d94c07b
iandersonalmeida <90912680+iandersonalmeida@users.noreply.github.com>
2022-11-11 13:50:51 -0300

    modify file TicketForm.jsx

--- src/components/TicketForm.jsx
+++ src/components/TicketForm.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 
 class TicketForm extends Component {
+    constructor(props){
+        super(props);
+        this.state = {ticketName: ''};
+    }
 
    
     render() {
         return (
             <div>
-                <h1>Ticket Compoment</h1>               
+                <h1>Ticket Compoment</h1>  
+
             </div>
         );
     }
