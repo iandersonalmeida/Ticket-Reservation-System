e3962e8600d3781ede23e04ddd1afb88bdfd4941
iandersonalmeida <90912680+iandersonalmeida@users.noreply.github.com>
2022-11-18 15:43:41 -0300

    add new file ticketform.css and  modify file TicketForm.jsx

--- src/components/TicketForm.jsx
+++ src/components/TicketForm.jsx
@@ -1,21 +1,36 @@
 import React, { Component } from 'react';
+import images from './images/images';
 
 class TicketForm extends Component {
     constructor(props){
         super(props);
         this.state = {ticketName: ''};
+
+        this.handleTextInput = this.handleTextInput.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    
+    handleTextInput(event){
+        this.setState({ticketName: event.target.value});
+        console.log("Ticket: ", this.state.ticketName);
     }
 
+    handleSubmit(event) {
+        alert('A name was submitted: '+ this.state.ticketName);
+        event.preventDefault();
+      }
    
     render() {
         return (
-            <div>
+            <div className='div1'>
                 <h1>Ticket Compoment</h1>  
-                     <form>
+                <img src={images.homepic} alt="phoenix logo" className="image"/> 
+                     <form onSubmit={this.handleSubmit}>
                         <label> 
                             Ticket Name:
-                            <input type="text" />
+                            <input type="text" value={this.state.ticketName} onChange={this.handleTextInput} />
                         </label>
+                        <button className='movieBtn' type='submit' value="Submit" >I am ready!</button>
                      </form>
 
             </div>
